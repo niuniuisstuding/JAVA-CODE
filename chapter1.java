ab890d6c9ef90a9d8d46dd569b570788b635a230
niuniuisstuding <2996674687@qq.com>
2022-03-04 16:50:58 +0800

    chapter1

---
+++ chapter1.java
@@ -0,0 +1,54 @@
+package niuniudejavadaima;
+
+public class chapter1 {
+
+//1-1  Welcome
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Java!");
+	}
+	
+
+	
+//1-2 WelcomeWithThreeMessages	
+/*	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Programming is run");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+	}
+*/
+	
+	
+//1-3 ComputeExpression	
+/*	public static void main(String[] args){
+		System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+	}
+	*/
+	
+
+//1-4 ShowSyntaxErrors
+/*	public static void main(String[] args){
+		System.out.println(Welcome to Java);//应加上""
+	}
+*/	
+	
+
+//1-5 ShowRuntimrErrors
+/*	public static void main(String[] args){
+		System.out.println(1 / 0);//运行时错误导致终止
+	}
+*/
+	
+	
+//1-6 ShowLogicErrors
+/*	public static void main(String[] args){
+	  System.out.print("Celsius 35 is Fahrenheit degree ");
+	  System.out.println((9 /5) * 35 + 32);//整数相除返回整数部分，小数部分被截掉
+	}
+*/
+	
+}
+
+
